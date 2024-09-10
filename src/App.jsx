2565d32c2799d47b7cfac90e8dba740ece67297b
Vishal <vishalgangwar8696@gmail.com>
2024-09-10 11:30:56 +0530

    project-init

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import List from './component/list';
+import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [post,setPost] = useState('');
 
+  useEffect(()=>{
+    async function getpost() {
+      const data = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPost(data);
+    }
+    getpost();
+  },[])
+  
   return (
-    <>
+    <>  
+      <div className='app-container'>
+          <h1>List of projects</h1>
+          {post&&<List post={post}></List>}
+      </div>
       
     </>
   )
 }
+
+
 
 export default App
